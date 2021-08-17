7cf4114b86ce91758db9f11add92b80f5dcb5836
Sumair1981 <smrkalim1@gmail.com>
2021-08-17 13:48:42 +0500

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>© 2020-2021 - Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="https://www.msumairk1981.work/">Muhammad Sumair Kaleem</Link></p>
+      <p>© 2020-2021 - Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="https://www.blog.msk1981.xyz/">Muhammad Sumair Kaleem</Link></p>
     </div>
   </footer>
 )
