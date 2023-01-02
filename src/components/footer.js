bcd28ffc139243479b9a01c47a0d774534b874f4
m.sumair kaleem <smrkalim1@gmail.com>
2023-01-02 16:56:44 +0500

    Updte footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ import { RiHeart2Line } from "react-icons/ri";
 const Footer = () => (
   <footer className="site-footer">
     <div className="container">
-      <p>© 2020-2022 - Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="https://www.coffeeeternal.com/">M.Sumair Kaleem</Link></p>
+      <p>© 2020-2023 - Made with <span className="icon -love"><RiHeart2Line/></span> by <Link to="https://www.coffeeeternal.com/">M.Sumair Kaleem</Link></p>
     </div>
   </footer>
 )
